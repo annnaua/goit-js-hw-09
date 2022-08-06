a3e32b0991247b9264e9199b765c1bae9ce6db46
Anna Riabova <annaua04@gmail.com>
2022-08-06 23:06:43 +0300

    task 1 completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,65 @@
+// REFERENCES
 
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+
+// FUNCTIONS
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBodyBackgroudColor() {
+  const backgroundColor = getRandomHexColor();
+  document.body.style.backgroundColor = backgroundColor;
+}
+
+const backgroundColorSwitcher = {
+  intervalID: null,
+  isActive: false,
+
+  startChangeColor() {
+    if (this.isActive) {
+      return;
+    }
+
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+
+    this.isActive = true;
+
+    this.intervalID = setInterval(() => {
+      changeBodyBackgroudColor();
+    }, 1000);
+  },
+
+  stopChangeColor() {
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+
+    this.isActive = false;
+
+    clearInterval(this.intervalID);
+  },
+};
+
+// EVENT LISTENRS
+
+refs.startBtn.addEventListener('click', () =>
+  backgroundColorSwitcher.startChangeColor()
+);
+refs.stopBtn.addEventListener('click', () =>
+  backgroundColorSwitcher.stopChangeColor()
+);
+
+// function startChangeColor() {
+//   backgroundColorSwitcher.startChangeColor();
+// }
+// function stopChangeColor() {
+//   backgroundColorSwitcher.stopChangeColor();
+// }
+
+// class BackgroundColorSwitcher {}
+// const BackgroundColorSwitcher = new backgroundColorSwitcher();
