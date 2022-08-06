0670d4be2d409bd56f6d31f17b2516887266ae51
Anna Riabova <annaua04@gmail.com>
2022-08-06 23:15:11 +0300

    task 1 completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -16,9 +16,13 @@ function changeBodyBackgroudColor() {
   document.body.style.backgroundColor = backgroundColor;
 }
 
-const backgroundColorSwitcher = {
-  intervalID: null,
-  isActive: false,
+// Class
+
+class BackgroundColorSwitcher {
+  constructor() {
+    this.intervalID = null;
+    this.isActive = false;
+  }
 
   startChangeColor() {
     if (this.isActive) {
@@ -33,7 +37,7 @@ const backgroundColorSwitcher = {
     this.intervalID = setInterval(() => {
       changeBodyBackgroudColor();
     }, 1000);
-  },
+  }
 
   stopChangeColor() {
     refs.startBtn.disabled = false;
@@ -42,8 +46,10 @@ const backgroundColorSwitcher = {
     this.isActive = false;
 
     clearInterval(this.intervalID);
-  },
-};
+  }
+}
+
+const backgroundColorSwitcher = new BackgroundColorSwitcher();
 
 // EVENT LISTENRS
 
@@ -54,12 +60,47 @@ refs.stopBtn.addEventListener('click', () =>
   backgroundColorSwitcher.stopChangeColor()
 );
 
+// const backgroundColorSwitcher = {
+//   intervalID: null,
+//   isActive: false,
+
+//   startChangeColor() {
+//     if (this.isActive) {
+//       return;
+//     }
+
+//     refs.startBtn.disabled = true;
+//     refs.stopBtn.disabled = false;
+
+//     this.isActive = true;
+
+//     this.intervalID = setInterval(() => {
+//       changeBodyBackgroudColor();
+//     }, 1000);
+//   },
+
+//   stopChangeColor() {
+//     refs.startBtn.disabled = false;
+//     refs.stopBtn.disabled = true;
+
+//     this.isActive = false;
+
+//     clearInterval(this.intervalID);
+//   },
+// };
+
+// EVENT LISTENRS
+
+// refs.startBtn.addEventListener('click', () =>
+//   backgroundColorSwitcher.startChangeColor()
+// );
+// refs.stopBtn.addEventListener('click', () =>
+//   backgroundColorSwitcher.stopChangeColor()
+// );
+
 // function startChangeColor() {
 //   backgroundColorSwitcher.startChangeColor();
 // }
 // function stopChangeColor() {
 //   backgroundColorSwitcher.stopChangeColor();
 // }
-
-// class BackgroundColorSwitcher {}
-// const BackgroundColorSwitcher = new backgroundColorSwitcher();
